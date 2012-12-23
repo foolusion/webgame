73f166b564277a39a49d143924fab1a39f4f01d7
Andrew O'Neill <foolusion@gmail.com>
2012-12-23 14:14:04 -0500

    input kinda works
    closed connection triggers log.fatal

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"html/template"
 	"io"
 	"log"
@@ -38,19 +38,81 @@ type socket struct {
 	done chan bool
 }
 
-func socketHandler(ws *websocket.Conn) {
-	s := socket{ws, make(chan bool)}
-	go func(s socket) {
-		ticker := time.NewTicker(time.Second)
-		finished := time.After(time.Second * 5)
-		for {
-			select {
-			case <-finished:
-				s.done <- true
-			case <-ticker.C:
-				fmt.Fprint(s, "hey")
+func (s socket) Close() error {
+	s.done <- true
+	return nil
+}
+
+func input(s io.ReadWriteCloser, p *Player) {
+	dec := json.NewDecoder(s)
+	enc := json.NewEncoder(s)
+	var m map[string]interface{}
+	for {
+		err := dec.Decode(&m)
+		switch {
+		case err == io.EOF:
+			s.Close()
+			return
+		case err != nil:
+			log.Fatal("Decode: ", err)
+		}
+
+		for k := range m {
+			if k != "move" {
+				delete(m, k)
 			}
 		}
-	}(s)
+		p.Move(m["move"].(string))
+		if err := enc.Encode(p); err != nil {
+			log.Fatal("Encode: ", err)
+		}
+	}
+}
+
+type Player struct {
+	Xpos int
+	Ypos int
+}
+
+func (p *Player) Move(direction string) {
+	switch direction {
+	case "left":
+		p.Xpos -= 1
+	case "right":
+		p.Xpos += 1
+	case "up":
+		p.Ypos -= 1
+	case "down":
+		p.Ypos += 1
+	case "up-left":
+		p.Xpos -= 1
+		p.Ypos -= 1
+	case "up-right":
+		p.Xpos += 1
+		p.Ypos -= 1
+	case "down-left":
+		p.Xpos -= 1
+		p.Ypos += 1
+	case "down-right":
+		p.Xpos += 1
+		p.Ypos += 1
+	}
+}
+
+func game(s io.ReadWriteCloser) {
+	player := Player{0, 0}
+	finished := time.After(time.Second * 45)
+	go input(s, &player)
+	for {
+		select {
+		case <-finished:
+			s.Close()
+		}
+	}
+}
+
+func socketHandler(ws *websocket.Conn) {
+	s := socket{ws, make(chan bool)}
+	go game(s)
 	<-s.done
 }
--- scripts/game.js
+++ scripts/game.js
@@ -12,11 +12,27 @@ oscan.width = TILE_WIDTH;
 oscan.height = TILE_HEIGHT;
 osctx = oscan.getContext('2d');
 
-var x = 0;
-var y = SCREEN_HEIGHT / 2 * TILE_HEIGHT;
+var inputMap = {};
+inputMap[33] = {'move': 'up-right'};
+inputMap[34] = {'move': 'down-right'};
+inputMap[35] = {'move': 'down-left'};
+inputMap[36] = {'move': 'up-left'};
+inputMap[37] = {'move': 'left'};
+inputMap[38] = {'move': 'up'};
+inputMap[39] = {'move': 'right'};
+inputMap[40] = {'move': 'down'};
+
+document.body.onkeydown = function(e) {
+//	console.log(e.which);
+	if (inputMap[e.which]) {
+		websocket.send(JSON.stringify(inputMap[e.which]));
+	}
+}
+
+var Player = {'Xpos': 0, 'Ypos': 0}
 
 function onMessage(e) {
-	console.log(e.data);
+	Player = JSON.parse(e.data);
 }
 
 function onClose(e) {
@@ -29,15 +45,13 @@ websocket.onclose = onClose;
 
 var canvas = document.getElementById('screen');
 var ctx = canvas.getContext('2d');
-ctx.fillStyle = "rgba(0, 255, 0, .5)";
+ctx.fillStyle = "rgb(0, 255, 0)";
 ctx.fillRect(0, 0, 800, 600);
 
 function render() {
-	x += 1;
-	x = x % ((SCREEN_WIDTH - 1) * TILE_WIDTH)
-	ctx.fillStyle = "rgba(0, 255, 0, .5)";
+	ctx.fillStyle = "rgb(0, 255, 0)";
 	ctx.fillRect(0, 0, 800, 600);
-	ctx.drawImage(oscan, x, y)
+	ctx.drawImage(oscan, Player.Xpos*TILE_WIDTH, Player.Ypos*TILE_HEIGHT)
 }
 
 window.requestAnimFrame = (function() {
